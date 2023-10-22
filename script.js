4f0fe5b12a72c3ef29e4679ec07d11d2bc18b59c
prettywoniu <prettywoiu@hotmail.com>
2023-10-22 11:41:13 -0700

    with a problem of alert

---
+++ script.js
@@ -0,0 +1,113 @@
+const gameContainer = document.getElementById("game");
+let card1 = null;
+let card2 = null;
+let cardsFlipped = 0;
+let noClicking = false;
+
+const COLORS = [
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple",
+  "red",
+  "blue",
+  "green",
+  "orange",
+  "purple"
+];
+
+// here is a helper function to shuffle an array
+// it returns the same array with values shuffled
+// it is based on an algorithm called Fisher Yates if you want ot research more
+function shuffle(array) {
+  let counter = array.length;
+
+  // While there are elements in the array
+  while (counter > 0) {
+    // Pick a random index
+    let index = Math.floor(Math.random() * counter);
+
+    // Decrease counter by 1
+    counter--;
+
+    // And swap the last element with it
+    let temp = array[counter];
+    array[counter] = array[index];
+    array[index] = temp;
+  }
+
+  return array;
+}
+
+let shuffledColors = shuffle(COLORS);
+
+// this function loops over the array of colors
+// it creates a new div and gives it a class with the value of the color
+// it also adds an event listener for a click for each card
+function createDivsForColors(colorArray) {
+  for (let color of colorArray) {
+    // create a new div
+    const newDiv = document.createElement("div");
+
+    // give it a class attribute for the value we are looping over
+    newDiv.classList.add(color);
+
+    // call a function handleCardClick when a div is clicked on
+    newDiv.addEventListener("click", handleCardClick);
+
+    // append the div to the element with an id of game
+    gameContainer.append(newDiv);
+  }
+}
+
+// TODO: Implement this function!
+function handleCardClick(event) {
+  if (noClicking) return;
+  if (event.target.classList.contains('flipped')) return;
+  
+  let currentCard = event.target;
+  currentCard.style.backgroundColor = currentCard.classList.value;
+  //console.log(currentCard.style.backgroundColor);
+
+  if (!card1 || !card2) {
+    currentCard.classList.add('flipped');  
+    card1 = card1 || currentCard;
+    card2 = currentCard === card1 ? null : currentCard;
+  }
+  //console.log(card1, card2)
+  //console.log(noClicking);
+  if (card1 && card2) {
+    noClicking = true;
+
+    const cn1 = card1.className;
+    const cn2 = card2.className;
+
+    if (cn1 === cn2){
+      cardsFlipped += 2;
+      card1.removeEventListener('click',handleCardClick);
+      card2.removeEventListener('click', handleCardClick);
+      card1 = null;
+      card2 = null;
+      noClicking = false;
+    } else {
+      setTimeout(function() {
+        //console.log(card1,card2);
+        card1.style.backgroundColor = "";
+        card2.style.backgroundColor = "";
+        card1.classList.remove('flipped');
+        card2.classList.remove('flipped');
+        card1 = null;
+        card2 = null;
+        noClicking = false;
+        //console.log(card1,card2)
+      }, 1000);
+    }   
+  }
+  //??????? Why alert first, then bkground color changed after click ok
+  if(cardsFlipped === COLORS.length) alert('Game Over!')
+
+}
+
+// when the DOM loads
+createDivsForColors(shuffledColors);
